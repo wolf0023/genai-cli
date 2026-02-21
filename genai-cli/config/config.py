2e451f1e77ebed293cee10b99df1aadf0cd1b1ed
wolf0023 <wolf0023@ymt.bbiq.jp>
2026-02-21 23:02:29 +0900

    feat: add a global configuration function via a settings file

---
+++ genai-cli/config/config.py
@@ -0,0 +1,98 @@
+from dataclasses import dataclass, asdict
+import os
+import json
+from jsonschema import validate, ValidationError
+
+from ui.chat_ui import ChatUI
+from const import CONFIG_DIR, CONFIG_FILE
+
+@dataclass
+class Config:
+    """ Configuration for genai-cli
+
+    Attributes:
+        default_model (str): The default model to use for chat interactions.
+        system_prompt (str): The system prompt to use for chat interactions.
+        history_len (int): The number of past messages to include in the chat history.
+        start_with_last_session (bool): Whether to start with the last session when launching the app.
+        your_name (str): The name to use for the user in chat interactions.
+    """
+    default_model: str = "gemini_pro"
+    system_prompt: str = "you are a helpful AI assistant."
+    history_len: int = 100
+    start_with_last_session: bool = True
+    your_name: str = "user"
+
+class ConfigManager:
+    """ Load and manage configuration settings.
+
+    Attributes:
+        config (Config): The loaded configuration settings.
+        config_path (str): The path to the configuration directory.
+        config_file (str): The path to the configuration file.
+        config_schema (str): The path to the configuration schema file.
+        chat_ui (ChatUI): The chat UI for displaying messages and errors.
+
+    Raises:
+        Exception: If there is an error loading the config file, an exception will be raised.
+    """
+    def __init__(self, chat_ui: ChatUI):
+        self.config: Config = Config()
+        self.config_path: str = os.path.expanduser(CONFIG_DIR)
+        self.config_file: str = os.path.join(self.config_path, CONFIG_FILE)
+        self.config_schema: str = os.path.join(os.path.dirname(__file__), 'config_schema.json')
+        self.chat_ui: ChatUI = chat_ui
+
+        os.makedirs(self.config_path, exist_ok=True)
+        self._load_config()
+
+    def _create_config_file(self):
+        """ Create a new configuration file with default settings.
+
+        Note:
+            This will overwrite any existing configuration file, so use with caution.
+
+        Raises:
+            Exception: If there is an error creating the config file, an exception will be raised.
+        """
+        try:
+            default_config = asdict(Config())
+            with open(self.config_file, 'w') as f:
+                json.dump(default_config, f, indent=4)
+
+        except Exception as e:
+            self.chat_ui.print_error(f"Failed to create config file: {e}")
+            raise
+
+    def _load_config(self):
+        """ Load configuration settings from file.
+
+        Raises:
+            Exception: If there is an error loading the config file, an exception will be raised.
+        """
+        # if config file doew not exitst, copy the default one
+        if not os.path.isfile(self.config_file):
+            self._create_config_file()
+
+        try:
+            # Load config from the configuration file
+            with open(self.config_file, 'r') as f:
+                configs = json.load(f)
+
+            # Load the config schema
+            with open(self.config_schema, 'r') as f:
+                schema = json.load(f)
+
+            # Validate the loaded config
+            validate(instance=configs, schema=schema)
+
+            # Overwrite Config object from loaded settings
+            self.config = Config(**configs)
+
+        except ValidationError as e:
+            self.chat_ui.print_error(f"Config validation error: {e.message}")
+            self.chat_ui.print_message("Loading default configuration settings.")
+
+        except Exception as e:
+            self.chat_ui.print_error(f"Failed to load config file: {e}")
+            raise
