5205bf062eeb98bdce39575f76d38ab9d4b20673
wolf0023 <wolf0023@ymt.bbiq.jp>
2026-04-20 19:24:59 +0900

    fix: delete unused DEFAULT message role

--- genai_cli/enums.py
+++ genai_cli/enums.py
@@ -3,7 +3,6 @@ from enum import StrEnum
 class Role(StrEnum):
     """ Enumeration of message roles
     """
-    DEFAULT = "user"
     SYSTEM = "system"
     USER = "user"
     ASSISTANT = "assistant"
